47d09b7e702379837f2389eec4ef25012bd2d195
Ulyana Pashina <pashinaulyana3@yandex.ru>
2023-08-16 22:49:49 +1100

    Do project

--- script/index.js
+++ script/index.js
@@ -18,6 +18,81 @@ const popupPhoto= document.querySelector('.popup-photo');
 const popupPhotoImage = popupPhoto.querySelector('.popup-photo__image');
 const popupPhotoFigurecaption = popupPhoto.querySelector('.popup-photo__figurecaption');
 const popupPhotoClosebutton = popupPhoto.querySelector('.popup-photo__closebutton');
+const closeButtons = document.querySelectorAll('.popup__close-button')
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+ 
+
+function showError(inputElement, errorElement) {
+  errorElement.textContent =  inputElement.validationMessage; 
+  inputElement.classList.add('form__input_type_error')
+};
+
+function hideError(inputElement, errorElement) {
+  errorElement.textContent =  inputElement.validationMessage; 
+  inputElement.classList.remove('form__input_type_error')
+  
+}; 
+
+
+function checkInputValidity(inputElement, formElement){
+  const isInputValid = inputElement.validity.valid;
+  const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
+  if (!isInputValid){
+    showError(inputElement, errorElement )
+  } else {
+    hideError(inputElement, errorElement)
+  }
+}
+
+function disableButton(buttonElement){
+  buttonElement.disabled =  "disabled";
+  buttonElement.classList.add('form__save-button_inactive')
+}
+
+function enableButton(buttonElement){
+  buttonElement.disabled =  "false";
+  buttonElement.classList.remove('form__save-button_inactive')
+}
+
+function toggleButtonState(buttonElement, isActive){
+  if(!isActive) {
+    disableButton(buttonElement)
+  }else{
+    enableButton(buttonElement)
+  }
+}
+
+
+
+function setEventListener(formElement){
+  const inputList = formElement.querySelectorAll('.form__input'); 
+  const submitbuttonElement = formElement.querySelector('.form__save-button');
+
+  toggleButtonState(submitbuttonElement, false);
+  [...inputList].forEach(function(inputElement){
+    inputElement.addEventListener('input', () =>{ 
+      checkInputValidity(inputElement, formElement);
+      toggleButtonState(submitbuttonElement, formElement.checkValidity());
+    })
+  })
+  formElement.addEventListener("submit", (evt) => {
+     evt.preventDefault()
+  })
+}
+
+function enableValidation() {
+  const formList = document.querySelectorAll('.form');
+  [...formList].forEach(function(formElement){
+    setEventListener(formElement)
+  })
+}
+
+enableValidation()
+
+
+
+
 
 const initialCards = [
   {
@@ -46,6 +121,12 @@ const initialCards = [
   }
 ];
 
+
+
+
+
+
+
 function openPopup(popup) {
   popup.classList.add('popup_opened')
 }
@@ -54,27 +135,27 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened')
 }
 
+
 editProfileButton.addEventListener('click', () => {
   openPopup(popupEdit)
 });
-closeEditButton.addEventListener('click', () => closePopup(popupEdit)) ;
+// closeEditButton.addEventListener('click', () => closePopup(popupEdit)) ;
 
 
 addProfileButton.addEventListener('click',() => openPopup(popupAdd));
-closeAddButton.addEventListener('click', () => closePopup(popupAdd));
+// closeAddButton.addEventListener('click', () => closePopup(popupAdd));
 
 
 
-
-function handleFormSubmit(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault(); 
 
-  document.querySelector('.profile__name').textContent = nameInput.value;
-  document.querySelector('.profile__description').textContent = jobInput.value;
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
   closePopup(popupEdit);
 }
 
-formEditElement.addEventListener('submit', handleFormSubmit); 
+formEditElement.addEventListener('submit', handleProfileFormSubmit); 
 
 function handleCreateCard(evt) {
   evt.preventDefault(); 
@@ -98,7 +179,7 @@ function createCard(name, link) {
     popupPhotoImage.src = link;
     popupPhotoImage.alt = name;
     popupPhotoFigurecaption.textContent = name;
-    popupPhotoClosebutton.addEventListener('click', () => closePopup(popupPhoto));
+    // popupPhotoClosebutton.addEventListener('click', () => closePopup(popupPhoto));
   });
   
 
@@ -120,6 +201,10 @@ initialCards.forEach(function(card){
   cards.prepend(createCard(card.name, card.link)); 
 })
 
+closeButtons.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+});
 
 
 
